15257fd900848c120ee3082275dde211cd36bad3
sssv078 <surajsri078@gmail.com>
2024-07-09 21:17:16 +0530

    initial commit

---
+++ src/main/java/com/swiggy/MagicArena/MagicArenaApplication.java
@@ -0,0 +1,13 @@
+package com.swiggy.MagicArena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiggy/MagicArena/MagicArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggy.MagicArena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
